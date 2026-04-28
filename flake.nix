0ea31d9c56204e7123131c79792307e71b0f939f
0xPlish <plish@hylo.so>
2026-04-28 14:27:45 -0700

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Solana dev environment for Hylo protocol";
+  inputs = {
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/6b70ae9e4f9738d69a7f1e5cdf05415ce233e358";
+    flake-parts.url =
+      "github:hercules-ci/flake-parts/3107b77cd68437b9a76194f0f7f9c55f2329ca5b";
+    rust-overlay.url =
+      "github:oxalica/rust-overlay/146e7bf7569b8288f24d41d806b9f584f7cfd5b5";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, rust-overlay }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        with import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        let
+          sharedBuildInputs = [ libiconv pkg-config gcc openssl ];
+          rustStable = rust-bin.stable."1.95.0".default.override {
+            extensions = [ "rust-analyzer" "rust-src" ];
+          };
+          rustPlatform = makeRustPlatform {
+            cargo = rustStable;
+            rustc = rustStable;
+          };
+          msig = rustPlatform.buildRustPackage {
+            pname = "msig";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = sharedBuildInputs;
+            buildInputs = [ openssl ]
+              ++ lib.optionals stdenv.isDarwin [ libiconv ];
+          };
+        in {
+          devShells.default = mkShell {
+            packages = [ rustStable ];
+            buildInputs = sharedBuildInputs;
+          };
+          packages.msig = msig;
+          packages.default = msig;
+        };
+    };
+}
