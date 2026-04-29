63f7ad978453954556e784d7a3ea2485ad134db8
0xPlish <plish@hylo.so>
2026-04-28 18:08:32 -0700

    PR feedback

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
-  description = "Solana dev environment for the msig CLI";
+  description = "Dev environment for the msig CLI";
   inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/6b70ae9e4f9738d69a7f1e5cdf05415ce233e358";
-    flake-parts.url =
-      "github:hercules-ci/flake-parts/3107b77cd68437b9a76194f0f7f9c55f2329ca5b";
-    rust-overlay.url =
-      "github:oxalica/rust-overlay/146e7bf7569b8288f24d41d806b9f584f7cfd5b5";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, rust-overlay }:
     flake-parts.lib.mkFlake { inherit inputs; } {
